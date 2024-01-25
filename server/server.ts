35b8f86b8b829903093eb28a2470d9eef7adb103
Samuel <s.looi2@outlook.com>
2024-01-25 21:33:41 +1300

    Reformatted code into GoogleLensOCR

--- server/server.ts
+++ server/server.ts
@@ -16,10 +16,54 @@ class GoogleLensOCR {
 			STRING - a string containing the text of the characters in the image
 	*/
 	constructor() { }
-	call(imagePath: string) {
+	async call(imagePath: string) {
+
+		const imageBlob = new Blob([await this.preprocess(imagePath)])
+		var file = new File([imageBlob], 'ocrImage.jpg', { type: 'image/jpeg' });
+
+		var formData = new FormData();
+		formData.append('encoded_image', file);
+
+		var time = new Date().getTime()
+		console.log("---   FETCHING   ---")
+		fetch(`https://lens.google.com/v3/upload?&stcs=${new Date().getTime()}`, {
+			method: 'POST',
+			body: formData
+		}).then(res => {
+			console.log("---   GOT RESPONSE!   ---")
+			return res.text()
+		}).then(text => {
+
+
+			const pattern = />AF_initDataCallback\({key: 'ds:1',.*?\)\;<\/script>/
+			const matchResult = text.match(pattern)
+			if (matchResult) {
+				// Get JSON from response
+				const codeBlockText = matchResult[0]
+				const frontFiltered = codeBlockText.substring(21)
+				const frontBackFiltered = frontFiltered.substring(0, frontFiltered.length - 11)
+				const lensResponseJSON = dJSON.parse(frontBackFiltered)
+
+
+				// If `errorHasStatus` field is `true`, then throw error
+				if (lensResponseJSON["errorHasStatus"]) throw new Error("errorHasStatus is true.Your image is probably too large, and must be shrunk to less than 1,000,000 pixels")
+				// If no text if found, length will be === 0
+				if (lensResponseJSON.data[3][4][0].length === 0) throw new Error("No text")
+
+				const textLines = lensResponseJSON.data[3][4][0][0]
+				console.log(textLines.join(" "))
+
+			} else {
+				console.log(pattern)
+				throw new Error("ERROR no match. ")
+			}
+
+			console.log(Math.round(((new Date().getTime()) - time) / 10) / 100, "seconds")
+		}).catch(err => { throw new Error(err) })
 
 	}
 	async preprocess(imagePath: string, MAX_PIXELS = 3000000) {
+		console.log("---   PRE-PROCESSING IMAGE   ---")
 		const file = await fsp.readFile(imagePath)
 
 		//####################
@@ -54,6 +98,11 @@ class GoogleLensOCR {
 				quality: 100,
 			})
 			.toBuffer()
+			.then(res => {
+
+				console.log("---   PRE-PROCESSING IMAGE   DONE   ---")
+				return res
+			})
 		// .then(() => {
 		// console.log("done!")
 		// })
@@ -61,53 +110,13 @@ class GoogleLensOCR {
 	}
 }
 
-var time = new Date().getTime()
+// var time = new Date().getTime()
 
 const googleLensOCR = new GoogleLensOCR()
-googleLensOCR.preprocess(path.join("assets", "saynotodrugs.png")).then(() => {
-	console.log(Math.round(((new Date().getTime()) - time) / 10) / 100, "seconds")
-})
-
-
-// const imageBlob = new Blob([fs.readFileSync(path.join(__dirname, "assets", "edit.jpg"))])
-// var file = new File([imageBlob], 'ocrImage.jpg', { type: 'image/jpeg' });
-
-// var formData = new FormData();
-// formData.append('encoded_image', file);
-
-// console.log("Fetching!")
-// var time = new Date().getTime()
-// fetch(`https://lens.google.com/v3/upload?&stcs=${new Date().getTime()}`, {
-// 	method: 'POST',
-// 	body: formData
-// }).then(res => {
-// 	console.log("GOT RESPONSE!")
-// 	return res.text()
-// }).then(text => {
-
-
-// 	const pattern = />AF_initDataCallback\({key: 'ds:1',.*?\)\;<\/script>/
-// 	const matchResult = text.match(pattern)
-// 	if (matchResult) {
-// 		// Get JSON from response
-// 		const codeBlockText = matchResult[0]
-// 		const frontFiltered = codeBlockText.substring(21)
-// 		const frontBackFiltered = frontFiltered.substring(0, frontFiltered.length - 11)
-// 		const lensResponseJSON = dJSON.parse(frontBackFiltered)
-
-
-// 		// If `errorHasStatus` field is `true`, then throw error
-// 		if (lensResponseJSON["errorHasStatus"]) throw new Error("errorHasStatus is true.Your image is probably too large, and must be shrunk to less than 1,000,000 pixels")
-// 		// If no text if found, length will be === 0
-// 		if (lensResponseJSON.data[3][4][0].length === 0) throw new Error("No text")
-
-// 		const textLines = lensResponseJSON.data[3][4][0][0]
-// 		console.log(textLines.join(" "))
-
-// 	} else {
-// 		console.log(pattern)
-// 		throw new Error("ERROR no match. ")
-// 	}
-
+// googleLensOCR.preprocess(path.join("assets", "saynotodrugs.png")).then(() => {
 // 	console.log(Math.round(((new Date().getTime()) - time) / 10) / 100, "seconds")
-// }).catch(err => { throw new Error(err) })
+// })
+
+
+googleLensOCR.call(path.join(__dirname, "assets", "edit.jpg"))
+// fs.readFileSync(path.join(__dirname, "assets", "edit.jpg"))
