9d5ddb6ea1d61aa296834b6c9f19bf79093dd0dc
Samuel <s.looi2@outlook.com>
2024-01-26 03:05:25 +1300

    Add check to shrink image's highest len to 1000 if exceed 3000000. Also reduce json parse time

--- server/server.ts
+++ server/server.ts
@@ -31,11 +31,12 @@ class GoogleLensOCR {
 	*/
 	constructor() { }
 	async call(imagePath: string) {
-		console.log("---   TIMER START   ---")
+		console.log("!!!   TIMER START   !!!")
 		timeLogger.start()
 
 
 		const file = await this.preprocess(imagePath)
+		timeLogger.lapAndLog()
 		var formData = new FormData();
 		formData.append('encoded_image', file);
 
@@ -44,39 +45,79 @@ class GoogleLensOCR {
 			method: 'POST',
 			body: formData
 		}).then(res => {
+			timeLogger.lapAndLog()
 			console.log("---   PROCESSING RESPONSE   ---")
 			return res.text()
-		}).then(text => {
+		}).then(async text => {
 
 
+			timeLogger.lapAndLog()
 			const pattern = />AF_initDataCallback\({key: 'ds:1',.*?\)\;<\/script>/
 			const matchResult = text.match(pattern)
 			if (matchResult) {
 				// Get JSON from response
 				const codeBlockText = matchResult[0]
-				const frontFiltered = codeBlockText.substring(21)
-				const frontBackFiltered = frontFiltered.substring(0, frontFiltered.length - 11)
-				const lensResponseJSON = dJSON.parse(frontBackFiltered)
+				const frontFiltered = codeBlockText.substring(21 + 30)
+				const frontBackFiltered = frontFiltered.substring(0, frontFiltered.length - (11 + 18))
+				await fsp.writeFile("asdfghjk.json", frontBackFiltered, { encoding: "utf-8" })
+				timeLogger.lapAndLog()
+				console.log("  # 1")
+				const lensResponseJSON = JSON.parse(frontBackFiltered)
+				timeLogger.lapAndLog()
+				console.log("  # 2")
 
 
 				// If `errorHasStatus` field is `true`, then throw error
 				if (lensResponseJSON["errorHasStatus"]) throw new Error("errorHasStatus is true.Your image is probably too large, and must be shrunk to less than 1,000,000 pixels")
 				// If no text if found, length will be === 0
-				if (lensResponseJSON.data[3][4][0].length === 0) throw new Error("No text")
+				if (lensResponseJSON[3][4][0].length === 0) throw new Error("No text")
 
-				const textLines = lensResponseJSON.data[3][4][0][0]
+				const textLines = lensResponseJSON[3][4][0][0]
 				timeLogger.lapAndLog()
 				console.log(textLines.join(" "))
 
 			} else {
 				console.log(pattern)
-				throw new Error("ERROR no match. ")
+				throw new Error("ERROR no match. You most likely have an post request with invalid data (eg: File with text/jpg instead of text/jpeg). ")
 			}
 
 		}).catch(err => { throw new Error(err) })
 
 	}
 	async preprocess(imagePath: string, MAX_PIXELS = 3000000) {
+		function findMaxPossibleDimensions(oldWidth, oldHeight, maxPixels) {
+			// Calculate scale
+			const scale = ((oldWidth * oldHeight) / maxPixels) ** 0.5
+
+			// Calculate new image dimensions
+			const newWidth = Math.floor(oldWidth / scale)
+			const newHeight = Math.floor(oldHeight / scale)
+
+			return [newWidth, newHeight]
+		}
+		function findDimensionsWhereLongestLengthIs(oldWidth, oldHeight, LONGEST_LENGTH = 1000) {
+			// Find if width or height is larger. We will set the larger dimension to 1000 and reduce the other dimension to keep the same previous aspect ratio
+			const aspectRatio = oldWidth / oldHeight
+
+
+			let newWidth = 0
+			let newHeight = 0
+			// Calculate new image dimensions
+			if (aspectRatio) {
+				// Width is larger than height
+				newWidth = LONGEST_LENGTH
+				const scale = LONGEST_LENGTH / oldWidth
+
+				newHeight = Math.floor(oldHeight * scale)
+			} else {
+				// Height is larger than width
+				newHeight = LONGEST_LENGTH
+				const scale = LONGEST_LENGTH / oldHeight
+
+				newWidth = Math.floor(oldWidth * scale)
+			}
+			return [newWidth, newHeight]
+		}
 		console.log("---   PRE-PROCESSING IMAGE   ---")
 		const file = await fsp.readFile(imagePath)
 
@@ -101,12 +142,7 @@ class GoogleLensOCR {
 		//####################
 		// Calculate new width and new height to ensure:   # of pixels in img is < MAX_PIXELS
 		//####################
-		// Calculate scale
-		const scale = ((oldWidth * oldHeight) / MAX_PIXELS) ** 0.5
-
-		// Calculate new image dimensions
-		const newWidth = Math.floor(oldWidth / scale)
-		const newHeight = Math.floor(oldHeight / scale)
+		const [newWidth, newHeight] = findDimensionsWhereLongestLengthIs(oldWidth, oldHeight)
 
 		//####################
 		// Reize Image
@@ -118,7 +154,6 @@ class GoogleLensOCR {
 			})
 			.toBuffer()
 			.then(res => {
-				timeLogger.lapAndLog()
 				return new File([new Blob([res])], 'ocrImage.jpg', { type: 'image/jpeg' });
 			})
 		// .then(() => {
@@ -136,5 +171,5 @@ const googleLensOCR = new GoogleLensOCR()
 // })
 
 
-googleLensOCR.call(path.join(__dirname, "assets", "a1.png"))
+googleLensOCR.call(path.join(__dirname, "assets", "w4.png"))
 // fs.readFileSync(path.join(__dirname, "assets", "edit.jpg"))
