257e8bcfde87d3fd6b003d2033a2b017223a35ee
s <s.looi2@outlook.com>
2024-01-26 17:31:20 +0900

    Implement captureArea

--- server/services/ScreenCapturer.ts
+++ server/services/ScreenCapturer.ts
@@ -2,6 +2,10 @@ import { TimeLogger } from "./utils/TimeLogger"
 const screenshot = require('screenshot-desktop')
 import fs from "fs"
 import path from "path"
+import sharp from "sharp";
+
+
+type RectangularShape = {x1:number,y1:number,x2:number,y2:number}
 
 // Create screen capturer
 export class ScreenCapturer {
@@ -9,6 +13,55 @@ export class ScreenCapturer {
 	async captureScreen(): Promise<Buffer> {
 		try {
 			const img = await screenshot() as Buffer
+			// fs.writeFileSync(path.join(__dirname,"..",".assets", "testing.jpg"), img);
+			return img
+		} catch (err) {
+			throw err
+		}
+	}
+	async captureArea(rectangularShape:RectangularShape): Promise<Buffer> {
+		function calcExtractArgs(area:RectangularShape){
+			/* 
+				Purpose:
+				- Receives some SHAPE and outputs arguments which can be used by Sharp's .extract method
+			*/
+
+			if(area.x1 === area.x2 || area.y1 === area.y2) throw new Error("ERROR: Width AND height must both have a length > 0")
+			
+			let top, left, width,height = 0
+
+			// Calculate vertical
+			if (area.y1<area.y2) {
+				top =area.y1 
+				height = area.y2 - area.y1
+			} else {
+				top =area.y2
+				height = area.y1 - area.y2 
+			}
+
+			// Calculate horizontal
+			if (area.x1<area.x2) {
+				left =area.x1
+				width = area.x2 - area.x1
+			} else {
+				left =area.x2
+				width = area.x1 - area.x2
+			}
+
+			return {
+				width,				
+				height,
+				left,
+				top
+			}
+		}
+
+
+
+		try {
+			const img = await screenshot() as Buffer
+			const image = await sharp(img)
+			image.extract(calcExtractArgs(rectangularShape)).toFile(path.join(__dirname,"..",".assets","crop.jpg"))
 			// fs.writeFileSync(path.join(__dirname,"..",".assets", "testing.jpg"), img);
 			return img
 		} catch (err) {
@@ -23,3 +76,4 @@ export class ScreenCapturer {
 // 	await new ScreenCapturer().captureScreen()
 // 	timeLogger.lap("Finished capturing")
 // })()
+
