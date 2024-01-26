26c19796b1026744fc350ca382938687ef1d4a50
Samuel <s.looi2@outlook.com>
2024-01-26 18:47:29 +1300

    Add basic chalk script

--- server/server.ts
+++ server/server.ts
@@ -4,3 +4,16 @@ import { GoogleLensOCR } from "./services/ocr"
 // Create GoogleLensOCR
 const googleLensOCR = new GoogleLensOCR()
 googleLensOCR.call(path.join(__dirname, "assets", "w9.png"))
+
+
+
+// (async function () {
+// 	const chalk = (await import('chalk')).default
+// 	console.log(chalk.blue('This is a blue message.'));
+// 	console.log(chalk.red.bold('This is a bold red message.'));
+// 	console.log(chalk.green.inverse('This is an inverted green message.'));
+
+// 	// Aligning text to the right
+// 	console.log(chalk.white.bgBlack('Aligned to the right').padStart(50));
+// 	console.log(chalk.white.bgBlack('Aligned to asd').padStart(50));
+// })()
