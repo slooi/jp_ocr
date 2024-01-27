60b5c3778fbedc0c8b9ffb456ba19ec2220b9433
Samuel <s.looi2@outlook.com>
2024-01-28 02:47:11 +1300

    Create basic file upload script

--- client/main.py
+++ client/main.py
@@ -1,8 +1,27 @@
 import keyboard
 import requests
+import pathlib
 
+# keyboard.add_hotkey('ctrl+shift+a', lambda:requests.get("http://localhost:54321"))
+# keyboard.wait()
 
-keyboard.add_hotkey('ctrl+shift+a', lambda:requests.get("http://localhost:54321"))
+import base64
+import json           
+import io         
 
+import requests
 
-keyboard.wait()
+api = 'http://localhost:54321/'
+image_file = pathlib.Path("assets","edit.jpg")
+
+with open(image_file, "rb") as f:
+    im_bytes = f.read()        
+
+# headers = {'Content-type': 'multipart/form-data', 'Accept': 'text/plain'}
+response = requests.post(api, files={'image': ('edit.jpg', io.BytesIO(im_bytes), 'image/jpeg')},timeout=5)
+try:
+    data = response.json()     
+    print(data)                
+except requests.exceptions.RequestException:
+    print(response.text)
+    
