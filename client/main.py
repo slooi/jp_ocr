a8027fe440c9253950a6848380f068eec5232add
Samuel <s.looi2@outlook.com>
2024-01-29 22:06:45 +1300

    Solve issue that caused errors on server due to incorrect format and non reset buffer

--- client/main.py
+++ client/main.py
@@ -6,7 +6,8 @@ import pathlib
 
 import base64
 import json           
-import io         
+import io
+from PIL import Image
 
 import requests
 
@@ -19,9 +20,14 @@ DEBUG_MODE = True
 # keyboard.wait()
 
 
-def post_image(url:str,image_path:pathlib.Path):
-	with open(image_path, "rb") as f:
-		im_bytes = f.read()
+def post_image(url:str,image_arg:pathlib.Path|bytes):
+	if type(image_arg) == bytes:
+		print("image_arg is bytes")
+		im_bytes = image_arg
+	else:
+		print("image_arg is path:",image_arg)
+		with open(image_arg, "rb") as f:
+			im_bytes = f.read()
 
 	response = requests.post(url, files={'image2': ('toOCR.jpg', io.BytesIO(im_bytes), 'image/jpeg')},timeout=5)
 	try:
@@ -31,23 +37,41 @@ def post_image(url:str,image_path:pathlib.Path):
 		print(response.text)
 		
 
-post_image('http://localhost:54321/',pathlib.Path("assets","edit.jpg"))
 
 def capture_screen():
 
 	with mss.mss() as sct:
 		# The screen part to capture
-		monitor = {"top": 0, "left": 0, "width": 300, "height": 300}
+		monitor = {"top": 0, "left": 0, "width": 300, "height": 1080}
 		output = str(pathlib.Path(".assets","sct-{top}x{left}_{width}x{height}.png".format(**monitor)))
-		print("output",output)
 
 		# Grab the data
 		sct_img = sct.grab(monitor)
-
+		
 		# Save to the picture file
 		if DEBUG_MODE:
 			mss.tools.to_png(sct_img.rgb, sct_img.size, output=output)
 			print("___DEBUG: Saved screenshot to file___")
-		print(output)
+			print(output)
 	
 
+	
+		img = Image.frombytes("RGB", sct_img.size, sct_img.rgb)
+		img_bytes = io.BytesIO()
+		img.save(img_bytes, format="jpeg")
+		img.save("asdas.jpg")
+		img_bytes.seek(0)
+
+		# with open("asd.jpg","wb") as f:
+		# 	f.write(img_bytes.read())
+	
+		return img_bytes.read()
+	
+
+
+# post_image('http://localhost:54321/',pathlib.Path("assets","edit.jpg"))
+
+post_image('http://localhost:54321/',capture_screen())
+# capture_screen()
+
+# post_image('http://localhost:54321/',pathlib.Path("asdas.jpg"))
