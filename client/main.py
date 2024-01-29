3236ac8089611f06b56fb149b7a0d79e0e138073
Samuel <s.looi2@outlook.com>
2024-01-29 17:15:36 +1300

    Make mypy stop throwing error and create basic code for client

--- client/main.py
+++ client/main.py
@@ -1,27 +1,52 @@
-import keyboard
 import requests
+import keyboard
 import pathlib
+
+
+
 
 # keyboard.add_hotkey('ctrl+shift+a', lambda:requests.get("http://localhost:54321"))
 # keyboard.wait()
 
-import base64
-import json           
-import io         
 
-import requests
+print("hgj hj kh")
 
-api = 'http://localhost:54321/'
-image_file = pathlib.Path("assets","edit.jpg")
+# import base64
+# import json           
+# import io         
 
-with open(image_file, "rb") as f:
-    im_bytes = f.read()        
+# import requests
 
-# headers = {'Content-type': 'multipart/form-data', 'Accept': 'text/plain'}
-response = requests.post(api, files={'image2': ('toOCR.jpg', io.BytesIO(im_bytes), 'image/jpeg')},timeout=5)
-try:
-    data = response.json()     
-    print(data)                
-except requests.exceptions.RequestException:
-    print(response.text)
+# api = 'http://localhost:54321/'
+# image_file = pathlib.Path("assets","edit.jpg")
+
+# with open(image_file, "rb") as f:
+#     im_bytes = f.read()        
+
+# # headers = {'Content-type': 'multipart/form-data', 'Accept': 'text/plain'}
+# response = requests.post(api, files={'image2': ('toOCR.jpg', io.BytesIO(im_bytes), 'image/jpeg')},timeout=5)
+# try:
+#     data = response.json()     
+#     print(data)                
+# except requests.exceptions.RequestException:
+#     print(response.text)
     
+
+
+
+
+import mss.tools
+
+with mss.mss() as sct:
+    # The screen part to capture
+    monitor = {"top": 160, "left": 160, "width": 160, "height": 135}
+    output = "sct-{top}x{left}_{width}x{height}.png".format(**monitor)
+
+    # Grab the data
+    sct_img = sct.grab(monitor)
+
+    # Save to the picture file
+    mss.tools.to_png(sct_img.rgb, sct_img.size, output=output)
+    print(output)
+    
+
