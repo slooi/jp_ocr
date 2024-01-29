da0c072b020ea1e468ac956519199263e5d97605
Samuel <s.looi2@outlook.com>
2024-01-29 20:50:02 +1300

    Oragnise imports and functional code into functions

--- client/main.py
+++ client/main.py
@@ -4,49 +4,56 @@ import pathlib
 
 
 
+import base64
+import json           
+import io         
+
+import requests
+
+
+import mss.tools
+
+DEBUG_MODE = True
 
 # keyboard.add_hotkey('ctrl+shift+a', lambda:requests.get("http://localhost:54321"))
 # keyboard.wait()
 
 
-print("hgj hj kh")
 
-# import base64
-# import json           
-# import io         
 
-# import requests
+def post_image():
+	api = 'http://localhost:54321/'
+	image_file = pathlib.Path("assets","edit.jpg")
 
-# api = 'http://localhost:54321/'
-# image_file = pathlib.Path("assets","edit.jpg")
+	with open(image_file, "rb") as f:
+		im_bytes = f.read()
 
-# with open(image_file, "rb") as f:
-#     im_bytes = f.read()        
-
-# # headers = {'Content-type': 'multipart/form-data', 'Accept': 'text/plain'}
-# response = requests.post(api, files={'image2': ('toOCR.jpg', io.BytesIO(im_bytes), 'image/jpeg')},timeout=5)
-# try:
-#     data = response.json()     
-#     print(data)                
-# except requests.exceptions.RequestException:
-#     print(response.text)
-    
+	# headers = {'Content-type': 'multipart/form-data', 'Accept': 'text/plain'}
+	response = requests.post(api, files={'image2': ('toOCR.jpg', io.BytesIO(im_bytes), 'image/jpeg')},timeout=5)
+	try:
+		data = response.json()
+		print(data)
+	except requests.exceptions.RequestException:
+		print(response.text)
+		
 
 
 
+def capture_screen():
 
-import mss.tools
+	with mss.mss() as sct:
+		# The screen part to capture
+		monitor = {"top": 0, "left": 0, "width": 300, "height": 300}
+		output = str(pathlib.Path(".assets","sct-{top}x{left}_{width}x{height}.png".format(**monitor)))
+		print("output",output)
 
-with mss.mss() as sct:
-    # The screen part to capture
-    monitor = {"top": 160, "left": 160, "width": 160, "height": 135}
-    output = "sct-{top}x{left}_{width}x{height}.png".format(**monitor)
+		# Grab the data
+		sct_img = sct.grab(monitor)
 
-    # Grab the data
-    sct_img = sct.grab(monitor)
-
-    # Save to the picture file
-    mss.tools.to_png(sct_img.rgb, sct_img.size, output=output)
-    print(output)
-    
+		# Save to the picture file
+		if DEBUG_MODE:
+			mss.tools.to_png(sct_img.rgb, sct_img.size, output=output)
+			print("___DEBUG: Saved screenshot to file___")
+		print(output)
+	
 
