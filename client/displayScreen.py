68163f911f04d4adffca8daa9b6c5fe160e72c55
Samuel <s.looi2@outlook.com>
2024-01-31 05:48:42 +1300

    Refactor GraphicsScene code into OCRCaptureApp for flatter code (composition over inheritence) and to decouple domain /business logic from dependencies

--- client/displayScreen.py
+++ client/displayScreen.py
@@ -6,36 +6,22 @@ from PySide6.QtGui import QPixmap, QColor
 
 
 class GraphicsScene(QGraphicsScene):
-	def __init__(self, parent=None):
-		super().__init__(parent)
+	def __init__(self,mousePressEventCallback,mouseMoveEventCallback):
+		super().__init__()
+		self.mousePressEventCallback = mousePressEventCallback
+		self.mouseMoveEventCallback = mouseMoveEventCallback
 
 		# Set Size
 		self.setSceneRect(0, 0, 1920, 1080)
-		
-
-		# Get screenshot
-		screen = QApplication.primaryScreen()
-		screenshot = screen.grabWindow(0)
-
-		# Load & Display Image 
-		image_item = QGraphicsPixmapItem(screenshot)
-		# image_item = QGraphicsPixmapItem(QPixmap("client/test2.png"))
-		self.addItem(image_item)
-
-
-		# Create selection area
-		self.selection_area = QGraphicsRectItem(0, 0, 50, 50)
-		self.selection_area.setBrush(QColor("blue"))
-		self.addItem(self.selection_area)
 
 	def add_item(self,item:QGraphicsPixmapItem):
 		self.addItem(item)
 		
 	def mousePressEvent(self, event):
-		self.selection_area.setRect(0,0,event.scenePos().x(),event.scenePos().y())
+		self.mousePressEventCallback(event)
 
 	def mouseMoveEvent(self, event: QGraphicsSceneMouseEvent) -> None:
-		self.selection_area.setRect(0,0,event.scenePos().x(),event.scenePos().y())
+		self.mouseMoveEventCallback(event)
 		# print(event.scenePos().x(),event.scenePos().y())
 
 class GraphicsView(QGraphicsView):
@@ -69,16 +55,49 @@ class OCRCaptureApp():
 
 
 		# Create graphics SCENE
-		self.graphics_scene = GraphicsScene()
+		self.graphics_scene = GraphicsScene(self.mouse_press_event,self.mouse_move_event)
 		# Create VIEW from SCENE
 		self.graphics_view = GraphicsView(self.graphics_scene)
+
+
+		# SETUP
+		self.setup()
+
 
 		# Create main window
 		self.main_window = MainWindow()
 		self.main_window.setCentralWidget(self.graphics_view)
 		# Full screen window
 		self.main_window.showFullScreen()
-		
+	
+	def setup(self):
+		self.add_screenshot()
+		self.add_rectangle_SETUP()
+
+	def add_screenshot(self):
+		# Get screenshot
+		screen = QApplication.primaryScreen()
+		screenshot = screen.grabWindow(0)
+
+		# Load & Display Image 
+		image_item = QGraphicsPixmapItem(screenshot)
+		# image_item = QGraphicsPixmapItem(QPixmap("client/test2.png"))
+
+		self.graphics_scene.addItem(image_item)
+
+	def add_rectangle_SETUP(self):
+		# Create selection area
+		self.selection_area = QGraphicsRectItem(0, 0, 50, 50)
+		self.selection_area.setBrush(QColor("blue"))
+
+		self.graphics_scene.addItem(self.selection_area)
+
+	def mouse_move_event(self,event):
+		self.selection_area.setRect(0,0,event.scenePos().x(),event.scenePos().y())
+	def mouse_press_event(self,event):
+		self.selection_area.setRect(0,0,event.scenePos().x(),event.scenePos().y())
+	def mouse_release_event(self):
+		pass
 	def run(self):
 		self.app.exec()
 
