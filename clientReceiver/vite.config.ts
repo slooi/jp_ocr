dcd61c8822102c89ba0d5ebdbf0e7f737b17bae4
Samuel <s.looi2@outlook.com>
2024-02-06 03:11:07 +1300

    Allow websockets to be proxied

--- clientReceiver/vite.config.ts
+++ clientReceiver/vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	server: {
+		proxy: {
+			"/": {
+				target: "ws://localhost:54321",
+				ws: true
+			},
+		}
+	}
 })
