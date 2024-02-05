e25aa03a1ae6501839e98e7fc17ade93f8209395
Samuel <s.looi2@outlook.com>
2024-02-06 03:11:34 +1300

    Add basic working websocket

--- clientReceiver/src/App.tsx
+++ clientReceiver/src/App.tsx
@@ -3,8 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+
+const ws = new WebSocket(location.origin.replace("http", "ws"))
+
 function App() {
 	const [count, setCount] = useState(0)
+
 
 	return (
 		<>
