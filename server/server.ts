147a9be4ddf880c6be4eac603a8c38a48f96f24b
Samuel <s.looi2@outlook.com>
2024-02-05 20:47:17 +1300

    Change import name to reflect naming change

--- server/server.ts
+++ server/server.ts
@@ -3,7 +3,7 @@ import { GoogleLensOCR } from "./services/ocr"
 import { ScreenCapturer } from "./services/ScreenCapturer"
 import express, { NextFunction, Request, Response } from "express"
 import multer from "multer"
-import { asyncNextCaller } from "./expressUtils"
+import { asyncNextCaller } from "./expressErrorsUtils"
 const PORT = 54321
 
 const upload = multer()
