1476dac0eb65c8b16eba9ac85c7b6820d7ff1f91
Samuel <s.looi2@outlook.com>
2024-02-06 02:55:49 +1300

    Add basic server and client for testing websockets

--- server/server.ts
+++ server/server.ts
@@ -4,6 +4,8 @@ import { ScreenCapturer } from "./services/ScreenCapturer"
 import express, { NextFunction, Request, Response } from "express"
 import multer from "multer"
 import { asyncNextCaller } from "./errorUtils"
+import ws from "ws"
+
 const PORT = 54321
 
 const upload = multer()
@@ -11,7 +13,6 @@ const app = express()
 app.use(express.json())
 // app.use(express.json({ limit: '50mb' }));
 // app.use(express.urlencoded({ limit: '50mb' }));
-
 
 
 
@@ -29,15 +30,27 @@ app.get("/", asyncNextCaller(async (req, res) => {
 	console.log("GoT IT!")
 	res.status(200).json(await controllerScreenToOCR())
 }))
+app.get("/websocket", asyncNextCaller(async (req, res) => {
+	res.status(200).sendFile(path.join(__dirname, "index.html"))
+}))
 app.post("/", upload.single('image2'), asyncNextCaller(async (req, res) => {
 	console.log("GoT IT! image")
 	if (!req.file) throw new Error("File was not uploaded in post!")
 
-
 	const googleLensOCR = new GoogleLensOCR({ DEBUG_MODE: true })
 	res.status(200).json(await googleLensOCR.call(req.file.buffer))
 }))
-app.listen(PORT, () => { console.log("Listening on port " + PORT) })
+const server = app.listen(PORT, () => { console.log("Listening on port " + PORT) })
+
+// WEBSOCKET SERVER
+const wsServer = new ws.Server({ server: server, path: "/" })
+wsServer.on("connection", ws => {
+	console.log("CONNECTION!")
+
+	ws.onmessage = (event) => {
+		console.log(event.data)
+	}
+})
 
 async function controllerScreenToOCR(): Promise<string> {
 	// Create ScreenCapturer
