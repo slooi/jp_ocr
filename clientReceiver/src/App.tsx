790d1b64649987248dfd46dc473c3cb90b2f2198
s <s.looi2@outlook.com>
2024-02-18 02:06:28 +0900

    Refactor code out from App into own component TextItem

--- clientReceiver/src/App.tsx
+++ clientReceiver/src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { RxClipboardCopy } from "react-icons/rx";
+import { TextItem } from './TextItem';
 
 const ws = new WebSocket(location.origin.replace("http", "ws") + "/websocket");
 
@@ -19,16 +20,12 @@ function App() {
 		htmlElement.scrollTop = htmlElement.scrollHeight - htmlElement.clientHeight
 	}, [textArray]);
 
-	const copyText = (text:string) => {
-		navigator.clipboard.writeText(text);
-	}
-
 	return (
 		<>
 			<div id="content">
 				{textArray.map(text => (
 				<p style={{display:"flex",alignItems:"end"}}>
-					<span style={{padding:"0.25rem",marginRight:"0.5rem"}} className='icon-wrapper'><RxClipboardCopy onClick={()=>copyText(text)}/></span><span style={{margin:"0.25rem 0"}}>{text}</span>
+					<TextItem text={text}/>
 				</p>
 				))}
 			</div>
---
+++ clientReceiver/src/TextItem.tsx
@@ -0,0 +1,31 @@
+
+import { HiCheck } from "react-icons/hi2";
+import { RxClipboardCopy } from "react-icons/rx";
+import {useState} from "react"
+
+export function TextItem({text}:{text:string}){
+	const [textWasCopied,setTextWasCopied] = useState(false)
+	
+	const copyText = (text:string) => {
+		// Write text to clipboard
+		navigator.clipboard.writeText(text);
+
+		// Set textWasCopied to true
+		setTextWasCopied(true)
+
+		setTimeout(()=>{
+			setTextWasCopied(false)
+		},1000)
+	}
+
+	return (
+		<>
+			<span style={{padding:"0.25rem",marginRight:"0.5rem"}} className='icon-wrapper' onClick={()=>copyText(text)}>
+				{
+					textWasCopied ? <HiCheck/> : <RxClipboardCopy/>
+				}
+			</span>
+			<span style={{margin:"0.25rem 0"}}>{text}</span>
+		</>
+	)
+}
