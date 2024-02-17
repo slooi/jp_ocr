ce147653ef5dd2e2c07d634605e5bc89e6f3c5dd
s <s.looi2@outlook.com>
2024-02-17 19:32:48 +0900

    Add server and client receiver to script

--- run_all.sh
+++ run_all.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-# Activate virtual environment
+# Change directory to server and run npm run dev
+cd server
+npm run dev &
+cd ..
+
+# Change directory to clientReceiver and run npm run dev
+cd clientReceiver
+npm run dev &
+cd ..
+
+# Activate virtual environment for Python
 source client/venv/Scripts/activate
 
 # Run Python file
-python client/main.py
+python client/main.py &
 
-# Deactivate virtual environment
+# Deactivate virtual environment after Python execution
 deactivate
