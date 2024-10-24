cc5fe0d1b6ad061cbc60f6008464db7a06c7ac36
s <s.looi2@outlook.com>
2024-10-24 21:19:13 +0900

    :sparkles: add new hotkey which captures name of speaker in gbfr

--- client/main.py
+++ client/main.py
@@ -25,6 +25,7 @@ if __name__ == "__main__":
 	LEFT_WIN_KEY = 91
 	A_KEY = 0x41
 	Z_KEY = 0x5A
+	S_KEY = 0x53
 	thread_pool.start(HotkeyRunnable(
 	[
 		Hotkey(modifiers=[LEFT_WIN_KEY],key=192,callback=lambda:region_capture()),
@@ -36,12 +37,18 @@ if __name__ == "__main__":
 	
 
 	
-		# GRANBLUE FANTASY - DIALOGUE
-		Hotkey(modifiers=[LEFT_WIN_KEY],key=Z_KEY,callback=lambda:signal_handler.signal.emit(lambda:ocr_capture_app.capture_region(TwoPoints(x1=960-450,y1=875,x2=960+450,y2=1040)))),
+		# GRANBLUE FANTASY - DIALOGUE (does capture name)
+		Hotkey(modifiers=[LEFT_WIN_KEY],key=Z_KEY,callback=lambda:signal_handler.signal.emit(lambda:ocr_capture_app.capture_region(TwoPoints(x1=450,y1=717,x2=960+450,y2=1040)))),
+		# GRANBLUE FANTASY - DIALOGUE (doesn't capture name)
+		# Hotkey(modifiers=[LEFT_WIN_KEY],key=Z_KEY,callback=lambda:signal_handler.signal.emit(lambda:ocr_capture_app.capture_region(TwoPoints(x1=960-450,y1=875,x2=960+450,y2=1040)))),
+		
 		# GRANBLUE FANTASY - POPUP HEADER
 		Hotkey(modifiers=[LEFT_ALT_KEY,LEFT_CONTROL_KEY],key=A_KEY,callback=lambda:signal_handler.signal.emit(lambda:ocr_capture_app.capture_region(TwoPoints(x1=460,y1=195,x2=1461,y2=280)))),
 		# GRANBLUE FANTASY - POPUP CONTENT
 		Hotkey(modifiers=[LEFT_ALT_KEY,LEFT_CONTROL_KEY],key=Z_KEY,callback=lambda:signal_handler.signal.emit(lambda:ocr_capture_app.capture_region(TwoPoints(x1=459,y1=606,x2=1460,y2=788)))),
+
+		# GRANBLUE FANTASY - CHOOSE DIALOGUE CENTER
+		Hotkey(modifiers=[LEFT_ALT_KEY,LEFT_CONTROL_KEY],key=S_KEY,callback=lambda:signal_handler.signal.emit(lambda:ocr_capture_app.capture_region(TwoPoints(x1=568,y1=340,x2=1345,y2=606)))),
 	]))
 
 	print("Setup done!")
